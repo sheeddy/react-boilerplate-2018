dd87bb5ccaebdcd1aeac6e6dfc6ee200f1ab3523
sheeddy <ing.sheeddy@gmail.com>
2018-03-22 12:23:27 -0400

    loaders webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
       }
     ]
   },
